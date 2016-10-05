b815b278c38085b4986034b38557139160312d58
Eric Colter <coltere@purdue.edu>
2016-10-05 17:47:50 -0400

    Need to add some memory safety, but the parser is now able to correctly instruct the symbol table manager on what to do.

--- src/symbol.hpp
+++ src/symbol.hpp
@@ -11,7 +11,9 @@ class Symbol{
 			float f;
 		};
 		enum Types { INT, STRING, FLOAT };
-		Symbol(std::string name, Types type, T value);
+		Symbol(std::string name, Types type, T value)
+		:Name(name), Type(type), Value(value)
+		{}
 		std::string Name;
 		Types Type;
 		T Value;

bc9119f02a5239575c79e22d89edaaf1c0fa3c66
Jordan P Huffaker <jhuffak@cparch05.ecn.purdue.edu>
2016-10-05 22:45:52 -0400

    Think that the table and symbol classes are working

--- src/table.cpp
+++ src/table.cpp
@@ -4,5 +4,38 @@ Table::Table(std::string _name)
 	:name(_name), hasBeenPrinted(false) { }
 
 void Table::InsertSymbol(Symbol sym) {
-	
+	symbols.push_back(sym);
+}
+
+Symbol* Table::GetSymbol(std::string _name) {
+	std::list<Symbol>::iterator it;
+	for (it = symbols.begin(); it != symbols.end(); it++) {
+		if (it->Name == _name) {
+			return &(*it);
+		}
+	}
+	return NULL;
+}
+
+void Table::PrintSymbols(std::stringstream& stream) {	
+	if (hasBeenPrinted)
+		return;
+
+	stream << "Symbol table " << name << std::endl;
+
+	std::list<Symbol>::iterator it;
+	for (it = symbols.begin(); it != symbols.end(); it++) {
+		std::string enumStr;
+		if (it->Type == Symbol::INT)
+			enumStr = "INT";
+		else if (it->Type == Symbol::STRING)
+			enumStr = "STRING";
+		else
+			enumStr = "FLOAT";
+		stream << "name " << it->Name << " type " << enumStr << std::endl;
+	}
+
+	stream << std::endl;
+
+	hasBeenPrinted = true;
 }

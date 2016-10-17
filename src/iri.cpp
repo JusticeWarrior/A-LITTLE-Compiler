4865cd49beeb64ddc9e19184309bb492e93dabf6
Jordan P Huffaker <jhuffak@ecegrid-thin6.ecn.purdue.edu>
2016-10-16 21:45:18 -0400

    Fixed iri to compile

--- src/iri.cpp
+++ src/iri.cpp
@@ -6,15 +6,18 @@ IRI::IRI(Types type, Operand op1)
 	}
 IRI::IRI(Types type, Operand op1, Operand op2)
 	:Type(type) {
-		Operands.push_back(op1, op2);
+		Operands.push_back(op1);
+		Operands.push_back(op2);
 	}
 IRI::IRI(Types type, Operand op1, Operand op2, Operand op3)
 	:Type(type) {
-		Operands.push_back(op1, op2, op3);
+		Operands.push_back(op1);
+		Operands.push_back(op2);
+		Operands.push_back(op3);
 	}
 
-IRI::PrintIRI(std::stringstream* stream) {
+void IRI::PrintIRI(std::stringstream* stream) {
 }
 
-IRI::PrintAssembly(std::stringstream* stream) {
+void IRI::PrintAssembly(std::stringstream* stream) {
 }

baf9f08e70159777973d05342c77924f5603748f
Eric Colter <coltere@purdue.edu>
2016-10-19 21:28:40 -0400

    Added a test script for step 4. It seems to pass, and there are no more memory leaks.  There shouldn't be any, even if the parse is aborted

---
+++ test3.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+make clean
+make
+
+exit_code=0
+
+run_test() {
+  echo "RUNNING FILE ${1}.micro =================="
+  cat testcases3/input/$1.micro | micro > test.out
+  testcases3/tiny test.out | head -n1 > test.result
+  if testcases3/tiny testcases3/output/$1.out | head -n1 | diff - test.result ; then
+    echo PASSED
+  else
+    echo "\033[0;31mFAILED\033[0m"
+    exit_code=1
+  fi
+}
+
+run_test test_expr
+run_test step4_testcase3

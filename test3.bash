70ef45ebd90516c10b141d232d0b13dcb778b31d
Eric Colter <coltere@purdue.edu>
2016-10-20 22:29:28 -0400

    Fixed the inclusion of some extreneous files

--- test3.bash
+++ test3.bash
@@ -8,9 +8,12 @@ run_test() {
   echo "RUNNING FILE ${1}.micro =================="
   cat testcases3/input/$1.micro | micro > test.out
   testcases3/tiny test.out | head -n1 > test.result
+  rm test.out
   if testcases3/tiny testcases3/output/$1.out | head -n1 | diff - test.result ; then
     echo PASSED
+    rm test.result
   else
+    rm test.result
     echo "\033[0;31mFAILED\033[0m"
     exit_code=1
   fi

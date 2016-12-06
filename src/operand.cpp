b98208f9d0f51a392e5fc8418930298fc6e5ddff
Jordan P Huffaker <jhuffak@ecegrid-thin2.ecn.purdue.edu>
2016-12-05 22:24:46 -0500

    Fixed a bug with typing

--- src/operand.cpp
+++ src/operand.cpp
@@ -6,23 +6,28 @@
 Operand::Operand() {
 	Type = NOTHING;
 	Awful = MAGIC;
+	AwfulType = MAGIC_TYPE;
 }
 Operand::Operand(Types type, std::string value)
-	:Type(type), Value(value), Awful(MAGIC) { }
+	:Type(type), Value(value), Awful(MAGIC), AwfulType(MAGIC_TYPE) { }
 Operand::Operand(Types type, int reg)
-	:Type(type), Reg(reg), Awful(MAGIC) { }
+	:Type(type), Reg(reg), Awful(MAGIC), AwfulType(MAGIC_TYPE) { }
+
 std::string Operand::ToString() {
+	if (AwfulType == MAGIC_TYPE)
+		AwfulType = Type;
 	if (Awful == MAGIC)
 	  Awful = Reg;
-	if (Type == NOTHING)
+
+	if (AwfulType == NOTHING)
 		return "";
-	else if (Type == REGISTER)
+	else if (AwfulType == REGISTER)
 		return "$T" + std::to_string(static_cast<long long>(Awful));
-	else if (Type == LOCAL)
+	else if (AwfulType == LOCAL)
 		return "$L" + std::to_string(static_cast<long long>(Awful));
-	else if (Type == PARAMETER)
+	else if (AwfulType == PARAMETER)
 		return "$P" + std::to_string(static_cast<long long>(Awful));
-	else if (Type == RETURN)
+	else if (AwfulType == RETURN)
 		return "$R";
 	else
 		return Value;
--- src/operand.hpp
+++ src/operand.hpp
@@ -5,7 +5,7 @@
 
 class Operand{
 	public:
-		enum Types { REGISTER, LITERAL, LABEL, VARIABLE, LOCAL, PARAMETER, RETURN, NOTHING};
+		enum Types { REGISTER, LITERAL, LABEL, VARIABLE, LOCAL, PARAMETER, RETURN, NOTHING, MAGIC_TYPE};
 		enum DataTypes {FLOAT, INT};
 		Operand();
 		Operand(Types type, std::string value);
@@ -13,6 +13,7 @@ class Operand{
 		std::string ToString();
 		std::string ToAssemblyString();
 		Types Type;
+		Types AwfulType;
 		DataTypes DataType;
 		std::string Value;
 		int Reg;    // IRI code
